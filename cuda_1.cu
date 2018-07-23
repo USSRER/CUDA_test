f662c77fd04911466d7acefc6f7604fa3f18d667
USSRER <847484392@qq.com>
2018-07-23 10:48:36 +0800

    10_48

--- cuda_1.cu
+++ cuda_1.cu
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
- void MatrixMultiplication(float *M,float * N,float *P,int Width){
+__global__ void MatrixMulKernel(float *Md,float *Nd,float *Pd,int Width){
+    int tx=threadIdx.x;
+    int ty=threadIdx.y;
+    float Pvalue=0;
+    for(int k=0;k<Width;k++){
+        float Mdelement=Md[ty*Width+k];
+        float Ndelement=Nd[k*Width+ty];
+        Pvalue+=Mdelement*Ndelement;
+        
+    }
+    Pd[ty*Width+tx]=Pvalue;
+}
+
+void MatrixMultiplication(float *M,float * N,float *P,int Width){
      int size=Width*Width*sizeof(float);
      float *Md,Nd,Pd;
      cudaMalloc((void **)&Md,size);
@@ -8,12 +21,25 @@ using namespace std;
      cudaMalloc((void **)&Nd,size);
      cudaMemcpy(Nd,N,size,cudaMemcpyHostToDevice);
      cudaMalloc((void **)&Pd,size)
+     dim3 dimBlock(Width,Width);
+     dim3 dimGrid(1,1);
+     MatrixMulKernel<<dimGrid,dimBlock>>(Md,Nd,Pd,Width);
+     cudaMemcpy(P,Pd,size,cudaMemcpyDeviceToHost);
+     cudafree(Md);
+     cudafree(Nd);
+     cudafree(Pd);
 
-
-
-    cudaMemcpy(P,Pd,size,cudaMemcpyDeviceToHost);
-    cudafree(Md);
-    cudafree(Nd);
-    cudafree(Pd);
-
+ }
+ int main(){
+     float M[3][3]={1,2,3,4,5,6,7,8,9};
+     float N[3][3]={9,8,7,6,5,4,3,2,1};
+     float P[3][3]={0};
+     MatrixMultiplication(*M,*N,*P,3);
+     cout << "P[3][3] = " << endl;
+     for(int m=0;m<3;m++){
+         for(int n=0;n<3,n++){
+             cout << P[m][n] << " ";
+         }
+         cout << endl;
+     }
  }
